1bd7a4dc0cb32ac5dd590a5181a6e0852e75c71f
Easton haaff <eastonahaaff1998@gmail.com>
2020-05-01 02:13:25 -0500

    added basic command handler

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 const Discord = require('discord.js')
 const config = require('./botConfig.json')
 const client = new Discord.Client()
+const commands = require('./commands')
+client.commands = new Discord.Collection();
+
+Object.keys(commands).map(key =>{
+    client.commands.set(commands[key].name, commands[key])
+})
 
 
 client.on('ready', () => {
     console.log('Boobles UPGRADED is online')
 })
 
+client.on('message', msg => {
+    const args = msg.content.split(/ +/)
+    const command = args.shift().toLowerCase()
+
+    if(!client.commands.has(command)) return
+    
+    try{
+        client.commands.get(command).execute(msg, args)
+    }catch(err){
+        console.error(err)
+        msg.reply('There was an error trying to call that command!')
+    }
+})
 client.login(config.TOKEN)
