f9059dabecc049f3fe0d675e677f01943e7da721
haaffin <eastonahaaff1998@gmail.com>
2020-07-20 15:28:53 -0500

    fixed help commands

--- commands/helpCommands/misc.js
+++ commands/helpCommands/misc.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 module.exports = {
     //make sure to include the ! as thats the prefix for the bot. You can change this to whatever you want, just make sure you use the same one
     //for every command
-    name: '!help', 
+    name: '!misc', 
     description: 'description',
     //For clarification, args and the client aren't required for every command, they're just here if you ever plan 
     //on using a command that requires them
